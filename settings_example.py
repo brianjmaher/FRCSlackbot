5ee3764ce83862d1eedf1a0d27c1e63093a3db76
maherb <maherb@rpi.edu>
2016-09-05 00:40:04 -0400

    Added template for settings.py

---
+++ settings_example.py
@@ -0,0 +1,10 @@
+# rename this to "settings.py" before use
+
+# Slack details
+SLACK_TOKEN = "YOUR_SLACK_API_TOKEN"
+SLACK_CHANNEL = "#channel_name"
+SLACK_USERNAME = "YourBotName"
+
+# other
+EVENT_ARCHIVE = "events.txt"
+LOOP_CYCLE = 15 * 60
